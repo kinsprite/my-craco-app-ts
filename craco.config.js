113f463228a68a017d4f669e27598de512b63265
kinsprite <qxqs11@126.com>
2021-07-03 22:12:55 +0800

    feat: craco配置更新

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,12 @@
-const changeOptimizationMinimize =
+const deepMerge = require('deepmerge');
+const { getPlugin, pluginByName } = require('@craco/craco');
+const getClientEnvironment = require('react-scripts/config/env');
+const rewireTypingsForCssModule = require('react-app-rewire-typings-for-css-module');
+const CracoAlias = require('craco-alias');
+// const CracoSassResourcesLoader = require('craco-sass-resources-loader');
+// const AddAssetPlugin = require('add-asset-webpack-plugin');
+
+const overrideOptimizationMinimize =
   minimize =>
   (webpackConfig, { env, paths }) => {
     if (minimize !== undefined) {
@@ -8,15 +16,60 @@ const changeOptimizationMinimize =
     return webpackConfig;
   };
 
+const overridePluginOptions =
+  (pluginName, options) =>
+  (webpackConfig, { env, paths }) => {
+    const { isFound, match } = getPlugin(webpackConfig, pluginByName(pluginName));
+
+    if (isFound && match.options !== undefined) {
+      match.options = deepMerge(match.options, options);
+    }
+
+    return webpackConfig;
+  };
+
+const overrideTypingsForCssModule =
+  () =>
+  (webpackConfig, { env, paths }) =>
+    rewireTypingsForCssModule(webpackConfig, env);
+
 const overrides =
   (...funcArray) =>
   (webpackConfig, { env, paths }) =>
     funcArray.reduce((config, func) => func(config, { env, paths }), webpackConfig);
 
 module.exports = {
-  webpack: {
-    configure: (webpackConfig, { env, paths }) => {
-      return overrides(changeOptimizationMinimize(false))(webpackConfig, { env, paths });
+  style: {
+    sass: {
+      loaderOptions: {
+        // Prefer 'sass' (dart-sass) over 'node-sass' if both packages are installed.
+        implementation: require('sass'),
+        // Workaround for this bug: https://github.com/webpack-contrib/sass-loader/issues/804
+        webpackImporter: false,
+      },
     },
   },
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      return overrides(
+        overrideOptimizationMinimize(undefined),
+        overridePluginOptions('HtmlWebpackPlugin', {
+          templateParameters: getClientEnvironment().raw,
+        }),
+        overrideTypingsForCssModule(),
+      )(webpackConfig, { env, paths });
+    },
+  },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'options',
+        baseUrl: './',
+        aliases: {
+          '@': './src',
+        },
+      },
+    },
+  ],
 };
